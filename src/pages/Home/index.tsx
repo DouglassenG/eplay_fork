03920a067832041c62c76968ef3cf1db8979b7ad
Douglas <douglasmsrodrigues@hotmail.com>
2025-10-22 12:13:58 -0300

    Setup

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,104 @@
+import Banner from '../../components/Banner'
+import ProductList from '../../components/ProductList'
+import Game from '../../models/Game'
+
+import resident from '../../assets/images/resident.png'
+import diablo from '../../assets/images/diablo.png'
+import zelda from '../../assets/images/zelda.png'
+import star_wars from '../../assets/images/star_wars.png'
+
+const promocoes: Game[] = [
+  {
+    id: 1,
+    category: 'Ação',
+    description:
+      'Resident Evil 4 é um jogo de terror de sobrevivência desenvolvido e publicado pela Capcom.',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident
+  },
+  {
+    id: 2,
+    category: 'Ação',
+    description:
+      'Resident Evil 4 é um jogo de terror de sobrevivência desenvolvido e publicado pela Capcom.',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['5%', 'R$ 290,00'],
+    image: resident
+  },
+  {
+    id: 3,
+    category: 'Ação',
+    description:
+      'Resident Evil 4 é um jogo de terror de sobrevivência desenvolvido e publicado pela Capcom.',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident
+  },
+  {
+    id: 4,
+    category: 'Ação',
+    description:
+      'Resident Evil 4 é um jogo de terror de sobrevivência desenvolvido e publicado pela Capcom.',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: diablo
+  }
+]
+
+const emBreve: Game[] = [
+  {
+    id: 5,
+    category: 'RPG',
+    description:
+      'Diablo 4 é um jogo de RPG de ação desenvolvido pela Blizzard Entertainment.',
+    title: 'zelda',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: zelda
+  },
+  {
+    id: 6,
+    category: 'RPG',
+    description:
+      'Diablo 4 é um jogo de RPG de ação desenvolvido pela Blizzard Entertainment.',
+    title: 'diablo',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: diablo
+  },
+  {
+    id: 7,
+    category: 'RPG',
+    description:
+      'Diablo 4 é um jogo de RPG de ação desenvolvido pela Blizzard Entertainment.',
+    title: 'zelda',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: zelda
+  },
+  {
+    id: 8,
+    category: 'RPG',
+    description:
+      'Diablo 4 é um jogo de RPG de ação desenvolvido pela Blizzard Entertainment.',
+    title: 'Star Wars',
+    system: 'Nintendo Switch',
+    infos: ['17/05'],
+    image: star_wars
+  }
+]
+
+const Home = () => (
+  <>
+    <Banner />
+    <ProductList games={promocoes} title="Promoções" background="black" />
+    <ProductList games={emBreve} title="Em breve" background="gray" />
+  </>
+)
+
+export default Home
